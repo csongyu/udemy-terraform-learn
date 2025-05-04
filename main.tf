1d5acba6dcc0207da172d29f497ea03ce2df7798
songor <chensongyu1024@gmail.com>
2025-05-04 14:36:00 +0800

    Route Table & Internet Gateway

--- main.tf
+++ main.tf
@@ -33,3 +33,21 @@ resource "aws_subnet" "my-subnet-1" {
     Name : "${var.environment}-subnet-1"
   }
 }
+
+resource "aws_internet_gateway" "my-internet-gateway" {
+  vpc_id = aws_vpc.my-vpc.id
+  tags = {
+    Name = "${var.environment}-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "my-route-table" {
+  vpc_id = aws_vpc.my-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my-internet-gateway.id
+  }
+  tags = {
+    Name = "${var.environment}-route-table"
+  }
+}
