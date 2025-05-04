1bf39f426103e9d2fe82b514ea25933926363241
songor <chensongyu1024@gmail.com>
2025-05-04 23:38:08 +0800

    Security Group

--- main.tf
+++ main.tf
@@ -67,3 +67,66 @@ resource "aws_default_route_table" "main-route-table" {
     Name = "${var.environment}-main-route-table"
   }
 }
+
+variable "my_cidr_ipv4" {
+
+}
+
+# resource "aws_security_group" "my-security-group" {
+#   name   = "my-security-group"
+#   vpc_id = aws_vpc.my-vpc.id
+#   tags = {
+#     Name = "${var.environment}-security-group"
+#   }
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_myself_ssh_ipv4" {
+#   security_group_id = aws_security_group.my-security-group.id
+#   cidr_ipv4         = var.my_cidr_ipv4
+#   from_port         = 22
+#   ip_protocol       = "tcp"
+#   to_port           = 22
+# }
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_all_http_ipv4" {
+#   security_group_id = aws_security_group.my-security-group.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 80
+#   ip_protocol       = "tcp"
+#   to_port           = 80
+# }
+
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+#   security_group_id = aws_security_group.my-security-group.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# }
+
+resource "aws_default_security_group" "default-security-group" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_cidr_ipv4]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.environment}-default-security-group"
+  }
+}
